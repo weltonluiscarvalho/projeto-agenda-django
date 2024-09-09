5b4b6658bea2d2b863dd937c9eb852cc26bc63f3
Welton <welton.carvalho.dev@gmail.com>
2024-09-09 08:25:20 -0300

    criar o model Category e fazer a ligação com a chave estrangeira

--- contact/admin.py
+++ contact/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from contact.models import Contact
+from contact.models import Contact, Category
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
@@ -11,3 +11,9 @@ class ContactAdmin(admin.ModelAdmin):
     list_max_show_all = 200
     list_editable = 'first_name', 'last_name'
     list_display_links = 'id', 'phone'
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
+    
