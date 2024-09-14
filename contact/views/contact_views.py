b043c30ad591ef9c778514659ff7e1928b8b6dff
Welton <welton.carvalho.dev@gmail.com>
2024-09-13 21:30:41 -0300

    incluir filtro nas buscas do Django

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from contact.models import Contact
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .order_by('-id')[10:20]
 
     context = {
         'contacts': contacts,
     }
+
     return render(
         request,
         'contact/index.html',
